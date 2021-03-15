abbee690adb72723bfe2dba55e1ca7bfa14d2d33
Lisa Oude Elferink <lisaoudeelferink@hotmail.com>
2021-03-15 14:54:40 +0100

    bug fix

--- tutorial/modules.js
+++
@@ -1,8 +0,0 @@
-const { people, ages } = require('./people');
-
-console.log(people, ages);
-
-// os stands for operating system
-const os = require('os');
-
-console.log(os.platform(), os.homedir());
